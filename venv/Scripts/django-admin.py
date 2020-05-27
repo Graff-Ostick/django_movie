10f6e60e24059181f5debc078f656b32fca7c6b5
Shebak Ruslan <rusya1998shebak@gmail.com>
2020-05-28 00:15:16 +0300

    create model

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\RUS_LAN\Desktop\python\django\folder_movi\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
