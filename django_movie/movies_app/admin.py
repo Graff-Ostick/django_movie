3c12a3975305e06bd2e5bc79fdab1ba4e4093e6d
Shebak Ruslan <rusya1998shebak@gmail.com>
2020-06-30 01:42:37 +0300

    uotput the list of last films, create tags - modul for return information from models.Movie

--- django_movie/movies_app/admin.py
+++ django_movie/movies_app/admin.py
@@ -1,14 +1,124 @@
 from django.contrib import admin
-from .models import  Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
-# Register your models here.
+from django.utils.safestring import mark_safe
+from django import forms
 
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(Actor)
-admin.site.register(Rating)
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MovieAdminForms(forms.ModelForm):
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+    class Meta():
+        model = Movie
+        fields = '__all__'
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
+class ReviewInLine(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ("email", "name")
+
+
+class MoviesShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
+
+    get_image.short_description = "Изображение"
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url", "draft",)
+    list_filter = ("category", "year")
+    search_fields = ("category__name", "title", "year")
+    readonly_fields = ("get_image",)
+    inlines = [MoviesShotsInline, ReviewInLine]
+    save_on_top = True
+    save_as = True
+    list_editable = ("draft",)
+    form = MovieAdminForms
+    #fields = (("actors", "directors", "genres"),)
+    fieldsets = (
+        (None,{
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": (("description", "poster", "get_image"),)
+        }),
+        (None, {
+            "fields": (("year", "world_premiere", "country"),)
+        }),
+        ("Actors", {
+            "classes":("collapse"),
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fees_in_world",),)
+        }),
+        ("Options", {
+            "fields": (("url", "draft",),)
+        }),
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
+
+    get_image.short_description = "Постер"
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ("movie", "name", "email", "parent", "id")
+    readonly_fields = ("email", "name")
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Genre"""
+    list_display = ("name", "url")
+
+
+@admin.register(MovieShots)
+class MovieShots(admin.ModelAdmin):
+    """Mivie Shots"""
+    list_display = ("title", "movie", "get_image")
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = "Изображение"
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Genre"""
+    list_display = ("name", "age", "get_image",)
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = "Изображение"
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Genre"""
+    list_display = ("movie", "ip",)
+
+
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
+
+admin.site.site_title = "Django Movies"
+admin.site.site_header = "Django Movies"
 
 
